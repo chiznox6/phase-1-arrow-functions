35a24b47b4006b9964c6dee43c5ca9e1d4878b08
James <james.kariuki5@student.moringaschool.com>
2025-06-15 12:08:05 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Declare a function using a function expression
+const divide = function() {
+  return 2000 / 100;
+};
+
+// Declare a function using arrow syntax
+const square = x => x * x;
+
+// Declare a function using arrow syntax with two parameters
+const add = (a, b) => a + b;
